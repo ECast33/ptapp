689db5babe69610ffda3de1bae1ab77c128d9612
Eduardo Castillo <ecast333@gmail.com>
2017-05-10 15:31:42 +0000

    added homepage route

--- frontend/app/config/app.config.js
+++ frontend/app/config/app.config.js
@@ -3,14 +3,19 @@
     //TODDO IMPL state provvider
     angular.module('app')
         .config([ '$locationProvider', '$stateProvider', '$urlRouterProvider', function ( $locationProvider, $stateProvider, $urlRouterProvider ) {
-            $urlRouterProvider.otherwise('/login');
+            $urlRouterProvider.otherwise('/home');
             $stateProvider
                 .state("login", {
                     url: "/login",
                     views:{
                         "content": {templateUrl:"app/login/login.html", controller: "logInController"}
                     }
-                });
+                }).state("home", {
+                    url: "/home",
+                    views:{
+                        "content": {templateUrl:"app/home/home.html", controller: "homeController"}
+                    }
+                });;
             $locationProvider.html5Mode({
                 enabled: true,
                 requireBase: true
